a15dea5faf91964c26135749bf7ff81e5481a81f
ThaHobbyist <dipdeys.dey@gmail.com>
2024-05-21 21:29:44 +0530

    fixed file directory of writer function

--- pyvnt/__init__.py
+++ pyvnt/__init__.py
@@ -1,6 +1,6 @@
 from pyvnt.Reference.basic import *
 from pyvnt.DictionaryElement.foamDS import *
 from pyvnt.DictionaryElement.keyData import *
-from pyvnt.Converter.writer import *
+from pyvnt.Converter.Writer.writer import *
 from pyvnt.utils import *
 from pyvnt.utils.showTree import *
