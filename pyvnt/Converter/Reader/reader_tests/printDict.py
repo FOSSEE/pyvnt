152631db0bdd6af354fa2d61d118d67173cf0fed
ThaHobbyist <dipdeys.dey@gmail.com>
2024-10-29 18:29:54 +0530

    fixed merge conflict

--- pyvnt/Converter/Reader/reader_tests/printDict.py
+++ pyvnt/Converter/Reader/reader_tests/printDict.py
@@ -1,7 +1,11 @@
 
 import os
 import sys
+<<<<<<< HEAD
 from pyvnt import read, show_tree, writeTo
+=======
+from pyvnt import read
+>>>>>>> acb2eb0 (fixed merge conflict)
 
 
 dictFilesFolder = os.path.join(os.path.dirname(__file__), 'dicts')
@@ -11,7 +15,12 @@ dictFile = os.path.join(dictFilesFolder, 'simpleDict')
 if len(sys.argv) > 1:
   dictFile = os.path.join(dictFilesFolder, sys.argv[1])
 
+<<<<<<< HEAD
 tree = read(dictFile)
 
 show_tree(tree)
 writeTo(tree, 'reader_test')
+=======
+tree = dw.read(dictFile)
+tree.dispTree()
+>>>>>>> acb2eb0 (fixed merge conflict)
