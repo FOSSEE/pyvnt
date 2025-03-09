4b6956524a80bd9a71e6744fb4334d5f94567f71
YashSuthar983 <yashsuthar983@gmail.com>
2025-03-10 01:24:38 +0530

    Added List_cp functioning

--- pyvnt/Converter/PlyParser/Parser.py
+++ pyvnt/Converter/PlyParser/Parser.py
@@ -47,7 +47,7 @@ def t_WORD(t):
     return t
 
 def t_NUMBER(t):
-    r'\d+(\.\d+)?(e[-+]?\d+)?'
+    r'-?\d+(\.\d+)?(e[-+]?\d+)?'
     t.value = float(t.value) if '.' in t.value or 'e' in t.value else int(t.value)
     return t
 
@@ -88,10 +88,64 @@ def p_blocks(p):
 
 def p_block(p):
     '''block : dictnary
+            | listblock
             | statement
+            | hex_item
+            | coordlists
             | empty'''
     p[0] = p[1]
 
+def p_listblock(p):
+    '''listblock : WORD LPAREN blocks RPAREN SEMICOLON'''
+    listcp=List_CP(p[1])
+    if isinstance(p[3][0],list):
+        i=0
+        for coord in p[3][0]:
+            coord._Value_P__name=f"v{i}"
+            i+=1
+    p[0]=Key_C(p[1],List_CP(p[1],elems=[p[3][0]]))
+    print(p[0])
+
+def p_coodlists(p):
+    '''coordlists : coordlists coodlist
+                  | coodlist
+    '''
+    if len(p)==3:
+        p[0]=p[1]+[p[2]]
+    else:
+        p[0]=[p[1]]
+
+def p_coordlist(p):
+    '''
+    coodlist : LPAREN NUMBER NUMBER NUMBER RPAREN
+    '''
+    p[0]=List_CP("v", elems=[[Flt_P('x', p[2]), Flt_P('y', p[3]), Flt_P('z', p[4])]])
+
+def p_hex_item(p):
+    '''hex_item : WORD LPAREN NUMBER NUMBER NUMBER NUMBER NUMBER NUMBER NUMBER NUMBER RPAREN LPAREN  NUMBER NUMBER NUMBER RPAREN WORD LPAREN  NUMBER NUMBER NUMBER RPAREN'''
+    p[0]=[Enm_P("type", {p[1]}, p[1]),
+            List_CP("faces", elems=[[
+                Int_P("v0", p[3]),
+                Int_P("v1", p[4]),
+                Int_P("v2", p[5]),
+                Int_P("v3", p[6]),
+                Int_P("v4", p[7]),
+                Int_P("v5", p[8]),
+                Int_P("v6", p[9]),
+                Int_P("v7", p[10])
+            ]]),
+            List_CP("res", elems=[[
+                Int_P("nx", p[13]),
+                Int_P("ny", p[14]),
+                Int_P("nz", p[15])
+            ]]),
+            Enm_P("grading", {"simpleGrading"}, "simpleGrading"),
+            List_CP("simpleGrading", elems=[[
+                Int_P("x", p[19]),
+                Int_P("y", p[20]),
+                Int_P("z", p[21])
+            ]])]
+    
 def p_dictnary(p):
     '''dictnary : WORD LBRACE blocks RBRACE'''
     node = Node_C(p[1])
@@ -151,119 +205,113 @@ def p_dimension(p):
     '''
     p[0]=Dim_Set_P("dim_set",p[2:9])
 
-def p_dictbody(p):
-    '''
-    dictbody : LBRACE statements RBRACE
-    '''
-    p[0]=p[2]
 
-    
-def p_statements(p):
-    '''statements : statements block
-                  | block'''
-    if len(p) == 3:
-        p[0] = p[1]+[p[2]]
-    else: 
-        p[0] = [p[1]]
 
-def p_statement_word_word(p):
-    '''statement : WORD words SEMICOLON
-    '''
-    #print(f"Parsing WORD-WORD: {p[1]}, {p[2]}")
-    p[0]=Key_C(p[1],Enm_P(p[1],set(p[2]),p[2][0]))
 
-def p_statement_word_words(p):
-    '''words : words WORD
-            | WORD 
-    '''
-    if len(p)==3:
-        p[1].append(p[2])
-        p[0]=p[1]
-    else:
-        p[0] = [p[1]]
 
-def p_statement_word_number(p):
-    '''statement : WORD NUMBER SEMICOLON'''
-    p[0]=Key_C(p[1],Flt_P('val1', minimum=0, maximum=1000, default=p[2]))
 
-def p_statement_dollar_word(p):
-    '''statement : DOLLAR WORD SEMICOLON'''
-    p[0] = {"$": p[2]}
 
-def p_statement_block(p):
-    '''statement : block'''
-    p[0] = p[1]
+# def p_statements(p):
+#     '''statements : statements block
+#                   | block'''
+#     if len(p) == 3:
+#         p[0] = p[1]+[p[2]]
+#     else: 
+#         p[0] = [p[1]]
 
-def p_statement_word_list(p):
-    '''statement : WORD value_list SEMICOLON'''
-    p[0] = {p[1]: p[2]}
+# def p_statement_word_word(p):
+#     '''statement : WORD words SEMICOLON
+#     '''
+#     #print(f"Parsing WORD-WORD: {p[1]}, {p[2]}")
+#     p[0]=Key_C(p[1],Enm_P(p[1],set(p[2]),p[2][0]))
 
-def p_list_block(p):
-    '''list_block : LPAREN list_items RPAREN SEMICOLON'''
-    p[0] = p[2]
-    #print(p[0])
+# def p_statement_word_words(p):
+#     '''words : words WORD
+#             | WORD 
+#     '''
+#     if len(p)==3:
+#         p[1].append(p[2])
+#         p[0]=p[1]
+#     else:
+#         p[0] = [p[1]]
 
-def p_list_items(p):
-    '''list_items : list_items list_item
-                  | list_item
-                  | blocks
-                  | empty'''
-    if len(p) == 3:
-        if p[1] is None:
-            p[0] = [p[2]]
-        else:
-            p[0] = p[1] + [p[2]] if p[2] is not None else p[1]
-    elif len(p) == 2 and p[1] is not None:
-        if isinstance(p[1],dict):
-            p[0]=p[1]
-        else:
-            p[0] = [p[1]]
-    else:
-        p[0] = []
+# def p_statement_word_number(p):
+#     '''statement : WORD NUMBER SEMICOLON'''
+#     p[0]=Key_C(p[1],Flt_P('val1', minimum=0, maximum=1000, default=p[2]))
 
-def p_list_item(p):
-    '''list_item : hex_item
-                 | faces_item'''
-    p[0] = p[1]
+# def p_statement_dollar_word(p):
+#     '''statement : DOLLAR WORD SEMICOLON'''
+#     p[0] = {"$": p[2]}
 
-def p_hex_item(p):
-    '''hex_item : WORD LPAREN vertex_list RPAREN LPAREN number_list RPAREN WORD LPAREN number_list RPAREN'''
-    # Specifically for the 'hex' block format
-    p[0] = {'type': p[1], 'vertices': p[3], 'dimensions': p[6], 'grading_type': p[8], 'gradings': p[10]}
+# def p_statement_block(p):
+#     '''statement : block'''
+#     p[0] = p[1]
 
-def p_faces_item(p):
-    '''faces_item : LPAREN vertex_list RPAREN'''
-    p[0] = p[2]
+# def p_statement_word_list(p):
+#     '''statement : WORD value_list SEMICOLON'''
+#     p[0] = {p[1]: p[2]}
 
-def p_vertex_list(p):
-    '''vertex_list : vertex_list NUMBER
-                   | NUMBER'''
-    if len(p) == 3:
-        p[0] = p[1] + [p[2]]
-    else:
-        p[0] = [p[1]]
+# def p_list_block(p):
+#     '''list_block : LPAREN list_items RPAREN SEMICOLON'''
+#     p[0] = p[2]
+#     #print(p[0])
 
-def p_number_list(p):
-    '''number_list : number_list NUMBER
-                   | NUMBER'''
-    if len(p) == 3:
-        p[0] = p[1] + [p[2]]
-    else:
-        p[0] = [p[1]]
+# def p_list_items(p):
+#     '''list_items : list_items list_item
+#                   | list_item
+#                   | blocks
+#                   | empty'''
+#     if len(p) == 3:
+#         if p[1] is None:
+#             p[0] = [p[2]]
+#         else:
+#             p[0] = p[1] + [p[2]] if p[2] is not None else p[1]
+#     elif len(p) == 2 and p[1] is not None:
+#         if isinstance(p[1],dict):
+#             p[0]=p[1]
+#         else:
+#             p[0] = [p[1]]
+#     else:
+#         p[0] = []
+
+# def p_list_item(p):
+#     '''list_item : hex_item
+#                  | faces_item'''
+#     p[0] = p[1]
+
+# def p_faces_item(p):
+#     '''faces_item : LPAREN vertex_list RPAREN'''
+#     p[0] = p[2]
+
+# def p_vertex_list(p):
+#     '''vertex_list : vertex_list NUMBER
+#                    | NUMBER'''
+#     if len(p) == 3:
+#         p[0] = p[1] + [p[2]]
+#     else:
+#         p[0] = [p[1]]
+
+# def p_number_list(p):
+#     '''number_list : number_list NUMBER
+#                    | NUMBER'''
+#     if len(p) == 3:
+#         p[0] = p[1] + [p[2]]
+#     else:
+#         p[0] = [p[1]]
 
 def p_empty(p):
     'empty :'
     p[0]=None
 
-def p_value_list(p):
-    '''value_list : value_list COMMA value_list
-                  | value'''
-    if len(p)==4:
-        p[0]=p[1]+p[3]
-    else:
-        p[0]=[p[1]]
+# def p_value_list(p):
+#     '''value_list : value_list COMMA value_list
+#                   | value'''
+#     if len(p)==4:
+#         p[0]=p[1]+p[3]
+#     else:
+#         p[0]=[p[1]]
 
-def p_value(p):
+# def p_value(p):
     '''value : WORD
              | NUMBER'''
     p[0] = p[1]
@@ -280,12 +328,53 @@ def parse_fvsolutions(text):
     return parser.parse(text, lexer=lexer)
 
 #Traverse through the folder
-for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
-    for file in os.listdir(file_path):
-        with open(os.path.join(file_path, file)) as tF:
-            text =tF.read()
-        tt=parse_fvsolutions(text)
-        #print(tt)
-        show_tree(tt)
+# for filename in os.listdir(folder_path):
+#     file_path = os.path.join(folder_path, filename)
+#     for file in os.listdir(file_path):
+#         with open(os.path.join(file_path, file)) as tF:
+#             text =tF.read()
+#         tt=parse_fvsolutions(text)
+#         #print(tt)
+#         show_tree(tt)
 
+tt= parse_fvsolutions(r'''/*--------------------------------*- C++ -*----------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Version:  9
+     \\/     M anipulation  |
+\*---------------------------------------------------------------------------*/
+FoamFile
+{
+    format      ascii;
+    class       dictionary;
+    object      blockMeshDict;
+}
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+convertToMeters 0.1;
+
+vertices
+(
+    (0 0 0)
+    (1 0 0)
+    (1 1 0)
+    (0 1 0)
+    (0 0 0.1)
+    (1 0 0.1)
+    (1 1 0.1)
+    (0 1 0.1)
+);
+
+blocks
+(
+    hex (0 1 2 3 4 5 6 7) (20 20 1) simpleGrading (1 1 1)
+);
+
+
+
+// ************************************************************************* //
+''')
+
+print (tt)
+show_tree(tt)
